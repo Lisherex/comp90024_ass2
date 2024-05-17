19aace8f85247ea69107028f5fda8614a6c8bd16
Ezra Tsai <68407336+EzraTsai@users.noreply.github.com>
2024-05-17 13:54:28 +1000

    Add local environment

--- backend/fission/functions/updateEapAllAirQualityMonitorSitesData/Main.py
+++ backend/fission/functions/updateEapAllAirQualityMonitorSitesData/Main.py
@@ -1,9 +1,19 @@
 import os
+from dotenv import load_dotenv
 from elasticsearch import Elasticsearch, helpers
 import urllib.request
 import urllib.parse
 import json
 from Config import Config
+
+load_dotenv()
+
+ALL_AIR_MONITORING_SITES_URL = os.getenv('ALL_AIR_MONITORING_SITES_URL')
+ELASTIC_SEARCH_URL = os.getenv('ELASTIC_SEARCH_URL')
+ELASTIC_SEARCH_PORT = os.getenv('ELASTIC_SEARCH_PORT')
+ES_USERNAME = os.getenv('ES_USERNAME')
+ES_PASSWORD = os.getenv('ES_PASSWORDc')
+EPA_APIKEY = os.getenv('EPA_APIKEY')
 
 class EnvironmentMonitorAPI:
     # def __init__(self, hdr: dict, baseURL) -> None:
@@ -62,51 +72,133 @@ def updateFunc():
         print(f"Handled unexpected error: {e}")
         raise
     # return data
+    # print ("test")
+    return json.dumps(data, indent=4)
 
-    url = configLoader("internal-service-ports", "ELASTIC_SEARCH_URL")
-    port = configLoader("internal-service-ports", "ELASTIC_SEARCH_PORT")
-    username = secretLoader("auth", "ES_USERNAME")
-    password = secretLoader("auth", "ES_PASSWORD")
-    es = Elasticsearch(f"{url}:{port}",
-                       verify_certs=False,
-                       basic_auth=(username, password)
-                       )
-    del url, port, username, password
-    # info = es.info().body
-    # return info
 
-    actions = []
-    for record in data.get("records", []):
-        site_id = record.get("siteID")
-        site_name = record.get("siteName")
-        coordinates = record.get("geometry", {}).get("coordinates")
-        if not coordinates:
-            continue
+    # url = configLoader("internal-service-ports", "ELASTIC_SEARCH_URL")
+    # port = configLoader("internal-service-ports", "ELASTIC_SEARCH_PORT")
+    # username = secretLoader("auth", "ES_USERNAME")
+    # password = secretLoader("auth", "ES_PASSWORD")
+    # es = Elasticsearch(f"{url}:{port}",
+    #                    verify_certs=False,
+    #                    basic_auth=(username, password)
+    #                    )
+    # del url, port, username, password
+    # # info = es.info().body
+    # # return info
+
+    # actions = []
+    # for record in data.get("records", []):
+    #     site_id = record.get("siteID")
+    #     site_name = record.get("siteName")
+    #     coordinates = record.get("geometry", {}).get("coordinates")
+    #     if not coordinates:
+    #         continue
         
-        for advice in record.get("siteHealthAdvices", []):
-            document_id = f"{site_id}_{advice.get("since")}"
-            action = {
-                "_index": "airquality",
-                "_id": document_id,
-                "_source": {
-                    "siteId": site_id,
-                    "siteName": site_name,
-                    "siteLocation": {"lat": coordinates[0], "lon": coordinates[1]},
-                    "parameter": advice.get("healthParameter"),
-                    "value": advice.get("averageValue"),
-                    "unit": advice.get("unit"),
-                    "healthAdvice": advice.get("healthAdvice"),
-                    "healthAdviceColor": advice.get("healthAdviceColor"),
-                    "since": advice.get("since"),
-                    "until": advice.get("until"),
-                }
-            }
-            actions.append(action)
+    #     for advice in record.get("siteHealthAdvices", []):
+    #         document_id = f"{site_id}_{advice.get("since")}"
+    #         action = {
+    #             "_index": "airquality",
+    #             "_id": document_id,
+    #             "_source": {
+    #                 "siteId": site_id,
+    #                 "siteName": site_name,
+    #                 "siteLocation": {"lat": coordinates[0], "lon": coordinates[1]},
+    #                 "parameter": advice.get("healthParameter"),
+    #                 "value": advice.get("averageValue"),
+    #                 "unit": advice.get("unit"),
+    #                 "healthAdvice": advice.get("healthAdvice"),
+    #                 "healthAdviceColor": advice.get("healthAdviceColor"),
+    #                 "since": advice.get("since"),
+    #                 "until": advice.get("until"),
+    #             }
+    #         }
+    #         actions.append(action)
     
-    if actions:
-        response = helpers.bulk(es, actions)
+    # if actions:
+    #     response = helpers.bulk(es, actions)
 
-    return response
+    # return response
+
+
+
+
+def updateFuncLocal():
+    configLoader = Config(True)
+    secretLoader = Config(False)
+
+    try:
+        data = EnvironmentMonitorAPI.getSiteData(
+            baseURL=ALL_AIR_MONITORING_SITES_URL,
+            params={"environmentalSegment": "air"},
+            hdr={
+                "Cache-Control": "no-cache",
+                "X-API-Key":EPA_APIKEY,
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+            }
+        )
+    except urllib.error.HTTPError as e:
+        print(f"Handled HTTP error: {e.code} {e.reason}")
+        raise
+    except urllib.error.URLError as e:
+        print(f"Handled URL error: {e.reason}")
+        raise
+    except Exception as e:
+        print(f"Handled unexpected error: {e}")
+        raise
+    # return data
+    # print ("test")
+    return json.dumps(data, indent=4)
+
+
+    # url = ELASTIC_SEARCH_URL
+    # port = ELASTIC_SEARCH_PORT
+    # username = ES_USERNAME
+    # password = ES_PASSWORD
+    # es = Elasticsearch(f"{url}:{port}",
+    #                    verify_certs=False,
+    #                    basic_auth=(username, password)
+    #                    )
+    # del url, port, username, password
+    # # info = es.info().body
+    # # return info
+
+    # actions = []
+    # for record in data.get("records", []):
+    #     site_id = record.get("siteID")
+    #     site_name = record.get("siteName")
+    #     coordinates = record.get("geometry", {}).get("coordinates")
+    #     if not coordinates:
+    #         continue
+        
+    #     for advice in record.get("siteHealthAdvices", []):
+    #         document_id = f"{site_id}_{advice.get("since")}"
+    #         action = {
+    #             "_index": "airquality",
+    #             "_id": document_id,
+    #             "_source": {
+    #                 "siteId": site_id,
+    #                 "siteName": site_name,
+    #                 "siteLocation": {"lat": coordinates[0], "lon": coordinates[1]},
+    #                 "parameter": advice.get("healthParameter"),
+    #                 "value": advice.get("averageValue"),
+    #                 "unit": advice.get("unit"),
+    #                 "healthAdvice": advice.get("healthAdvice"),
+    #                 "healthAdviceColor": advice.get("healthAdviceColor"),
+    #                 "since": advice.get("since"),
+    #                 "until": advice.get("until"),
+    #             }
+    #         }
+    #         actions.append(action)
+    
+    # if actions:
+    #     response = helpers.bulk(es, actions)
+
+    # return response
+
+
+
 
 def handler():
     try:
@@ -114,3 +206,14 @@ def handler():
         return {"status": "success", "data": data}
     except Exception as e:
         return {"status": "error", "message": str(e)}
+
+
+def handlerLocal():
+    try:
+        data = updateFuncLocal()
+        return {"status": "success", "data": data}
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
+
+if __name__ == "__main__":
+    handler()
