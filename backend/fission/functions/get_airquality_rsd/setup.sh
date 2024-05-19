c1ff157204d02b9efcc7bb6813fd9f2aef5f0d55
henry22 <henry811010@gmail.com>
2024-05-19 18:22:34 +1000

    Update setup shell script

--- backend/fission/functions/get_airquality_rsd/setup.sh
+++ backend/fission/functions/get_airquality_rsd/setup.sh
@@ -6,14 +6,14 @@ FISSION_NAMESPACE=default
 
 ENV_NAME="python"
 
-PKG_NAME="airquality-rsd"
+PKG_NAME="get-airquality-rsd"
 
-FN_NAME="airquality-rsd"
+FN_NAME="get-airquality-rsd"
 FN_ENTRYPOINT="get_airquality_rsd.main"
 
 ROUTE_NAME="airquality-rsd"
 ROUTE_METHOD="GET"
-ROUTE_URL="/airquality-rsd"
+ROUTE_URL="/airquality/rsd"
 ROUTE_CREATEINGRESS="TRUE"
 #####################################
 
