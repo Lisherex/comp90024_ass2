9b0561f3ea9a1e5516087df253ec064067502ba1
henry22 <henry811010@gmail.com>
2024-05-20 13:44:39 +1000

    Update get_houseprice_vehicle

--- backend/fission/functions/get_houseprice_vehicle/get_houseprice_vehicle.py
+++ backend/fission/functions/get_houseprice_vehicle/get_houseprice_vehicle.py
@@ -3,42 +3,11 @@ import json
 import os
 import logging
 from collections import defaultdict
-from Config import Config
+from config import Config
 
 # Configure logging
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-
-# def calculate_average_copd_admissions(copd_hits):
-#     admissions_data = defaultdict(lambda: defaultdict(list))
-
-    
-
-#     for hit in copd_hits:
-#         station_id = hit['_source']['station_id']
-#         # Replace None with 0 using the `or 0` pattern
-#         total = hit['_source'].get('admissions_chronic_obstructive_pulmonary_disease_public_hospital_total_number', 0) or 0
-#         males = hit['_source'].get('admissions_chronic_obstructive_pulmonary_disease_public_hospital_males_number', 0) or 0
-#         females = hit['_source'].get('admissions_chronic_obstructive_pulmonary_disease_public_hospital_females_number', 0) or 0
-
-#         admissions_data[station_id]['total'].append(total)
-#         admissions_data[station_id]['males'].append(males)
-#         admissions_data[station_id]['females'].append(females)
-
-#     average_admissions = {}
-#     for station_id, categories in admissions_data.items():
-#         total_count = len(categories['total'])
-#         male_count = len(categories['males'])
-#         female_count = len(categories['females'])
-
-#         average_admissions[station_id] = {
-#             'average_copd_total': sum(categories['total']) / total_count if total_count else 0,
-#             'average_copd_males': sum(categories['males']) / male_count if male_count else 0,
-#             'average_copd_females': sum(categories['females']) / female_count if female_count else 0
-#         }
-
-#     return average_admissions
-    
 
 def fetch_vehicle_data(client):
     res = client.search(
@@ -71,12 +40,12 @@ def merge_data(avg_house_price_hits, vehicle_hits):
         station_id = avg_hit['_source']['station_id']
         avg_price = avg_hit['_source']['average_price']
         matching_vehicle_hit = None
-        
+
         for vehicle_hit in vehicle_hits:
             if vehicle_hit['_source']['station_id'] == station_id:
                 matching_vehicle_hit = vehicle_hit
                 break
-        
+
         if matching_vehicle_hit:
             vehicles = matching_vehicle_hit['_source']
             merged_data.append({
@@ -92,27 +61,7 @@ def merge_data(avg_house_price_hits, vehicle_hits):
                 "total_dwelling": vehicles['total_dwelling']
             })
 
-    # for hit in avg_house_price_hits:
-    #     station_id = hit['_source']['station_id']
-    #     avg_price = hit['_source']['average_price']
-    #     if station_id in vehicle_hits['_source']:
-    #         vehicles = vehicle_hits['_source'][station_id]
-    #         merged_data.append({
-    #             "station_id": station_id,
-    #             "average_price": avg_price,
-    #             "lga_name": vehicles['_source']['lga_name'],
-    #             "lga_code": vehicles['_source']['lga_code'],
-    #             "none_vehicle_per_dwelling": vehicles['_source']['none_vehicle_per_dwelling'],
-    #             "one_vehicle_per_dwelling": vehicles['_source']['one_vehicle_per_dwelling'],
-    #             "two_vehicle_per_dwelling": vehicles['_source']['two_vehicle_per_dwelling'],
-    #             "three_vehicle_per_dwelling": vehicles['_source']['three_vehicle_per_dwelling'],
-    #             "more_than_four_vehicle_per_dwelling": vehicles['_source']['more_than_four_vehicle_per_dwelling'],
-    #             "total_dwelling": vehicles['_source']['total_dwelling']
-    #         })
-
     return merged_data
-
-
 
 def main():
     configLoader = Config(True)
@@ -122,6 +71,7 @@ def main():
     port = configLoader("internal-service-ports", "ELASTIC_SEARCH_PORT")
     username = secretLoader("auth", "ES_USERNAME")
     password = secretLoader("auth", "ES_PASSWORD")
+
     try:
         client = Elasticsearch(
             f'{url}:{port}',
@@ -137,11 +87,12 @@ def main():
 
         # Merge data
         merged_results = merge_data(avg_house_price_hits, vehicle_hits)
-        return json.dumps(merged_results, indent=4)
 
+        return json.dumps(merged_results, indent=4)
 
     except Exception as e:
         logger.error(f"Error connecting to Elasticsearch: {e}")
 
 # if __name__ == "__main__":
-#     main()
+#     data = main()
+#     print(data)
