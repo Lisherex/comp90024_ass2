682e9f693cba8418444c7aba1f9fb9dfae4ff6a8
EraPF <992939753@qq.com>
2024-05-21 18:20:19 +1000

    update 2nd-end2end.py

--- test/2nd-iteration/end2end.py
+++ test/2nd-iteration/end2end.py
@@ -1,6 +1,5 @@
 import unittest
 import requests
-import json
 from unittest.mock import patch
 
 class HTTPSession:
@@ -21,56 +20,66 @@ class HTTPSession:
         return self.session.delete(f'{self.base_url}/{path}')
 
 class TestEnd2End(unittest.TestCase):
-    #Mocking External Services
+    @classmethod
+    def setUpClass(cls):
+        cls.test_request = HTTPSession('http', 'localhost', 9090)
+
+    # Mocking External Services
     def test_airquality_copd(self):
-        self.assertEqual(test_request.get('/airquality/copd').status_code, 200)
-        self.assertIsNotNone(test_request.get('/airquality/copd').json())
+        response = self.test_request.get('/airquality/copd')
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(response.json())
 
     def test_airquality_rsd(self):
-        self.assertEqual(test_request.get('/airquality/rsd').status_code, 200)
-        self.assertIsNotNone(test_request.get('/airquality/rsd').json())
+        response = self.test_request.get('/airquality/rsd')
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(response.json())
 
     def test_airquality_vehicle(self):
-        self.assertEqual(test_request.get('/airquality/vehicle').status_code, 200)
-        self.assertIsNotNone(test_request.get('/airquality/vehicle').json())
+        response = self.test_request.get('/airquality/vehicle')
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(response.json())
 
     def test_airquality_houseprice(self):
-        self.assertEqual(test_request.get('/airquality/houseprice').status_code, 200)
-        self.assertIsNotNone(test_request.get('/airquality/houseprice').json())
+        response = self.test_request.get('/airquality/houseprice')
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(response.json())
 
     def test_houseprice_copd(self):
-        self.assertEqual(test_request.get('/houseprice/copd').status_code, 200)
-        self.assertIsNotNone(test_request.get('/houseprice/copd').json())
+        response = self.test_request.get('/houseprice/copd')
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(response.json())
 
     def test_houseprice_vehicle(self):
-        self.assertEqual(test_request.get('/houseprice/vehicle').status_code, 200)
-        self.assertIsNotNone(test_request.get('/houseprice/vehicle').json())
+        response = self.test_request.get('/houseprice/vehicle')
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(response.json())
 
-    #Test content type and headers verification
+    # Test content type and headers verification
     def test_content_type_for_copd(self):
         response = self.test_request.get('/airquality/copd')
-        self.assertIn('application/json', response.headers['Content-Type'])
+        self.assertIn('text/html; charset=utf-8', response.headers['Content-Type'])
 
     def test_content_type_for_rsd(self):
         response = self.test_request.get('/airquality/rsd')
-        self.assertIn('application/json', response.headers['Content-Type'])
+        self.assertIn('text/html; charset=utf-8', response.headers['Content-Type'])
 
     def test_content_type_for_vehicle(self):
         response = self.test_request.get('/airquality/vehicle')
-        self.assertIn('application/json', response.headers['Content-Type'])
+        self.assertIn('text/html; charset=utf-8', response.headers['Content-Type'])
 
     def test_content_type_for_houseprice(self):
         response = self.test_request.get('/airquality/houseprice')
-        self.assertIn('application/json', response.headers['Content-Type'])
+        self.assertIn('text/html; charset=utf-8', response.headers['Content-Type'])
 
     def test_content_type_for_houseprice_copd(self):
         response = self.test_request.get('/houseprice/copd')
-        self.assertIn('application/json', response.headers['Content-Type'])
+        self.assertIn('text/html; charset=utf-8', response.headers['Content-Type'])
 
     def test_content_type_for_houseprice_vehicle(self):
         response = self.test_request.get('/houseprice/vehicle')
-        self.assertIn('application/json', response.headers['Content-Type'])
+        self.assertIn('text/html; charset=utf-8', response.headers['Content-Type'])
+
 
 if __name__ == '__main__':
-    test_request = HTTPSession('http', 'localhost', 9090)
     unittest.main()
